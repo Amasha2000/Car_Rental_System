a04b64ce9922d0e698450c6585b533e38e5885d7
Amasha2000 <amashagalhenage@gmail.com>
2022-07-11 19:10:50 +0530

    Content updated in Config classes

--- CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/config/WebRootConfig.java
+++ CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/config/WebRootConfig.java
@@ -1,4 +1,8 @@
 package lk.easyCarRental.spring.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
 public class WebRootConfig {
 }
