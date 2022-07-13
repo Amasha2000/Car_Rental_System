f2ca844e65534aceff80f35d552fbd77d9f731ee
Amasha2000 <amashagalhenage@gmail.com>
2022-07-13 14:11:35 +0530

    Added DriverService and Impl

---
+++ CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/service/impl/DriverServiceImpl.java
@@ -0,0 +1,34 @@
+package lk.easyCarRental.spring.service.impl;
+
+import lk.easyCarRental.spring.dto.DriverDTO;
+import lk.easyCarRental.spring.service.DriverService;
+
+import java.util.ArrayList;
+
+
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public void saveDriver(DriverDTO driverDTO) {
+
+    }
+
+    @Override
+    public void updateDriver(DriverDTO driverDTO) {
+
+    }
+
+    @Override
+    public void deleteDriver(String id) {
+
+    }
+
+    @Override
+    public DriverDTO searchDriver(String id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<DriverDTO> getAllDrivers() {
+        return null;
+    }
+}
