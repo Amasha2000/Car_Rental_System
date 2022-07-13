afea9d116b11ea97685dcdbc74147ce584355bba
Amasha2000 <amashagalhenage@gmail.com>
2022-07-13 14:34:00 +0530

    Content Updated

--- CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/service/DriverService.java
+++ CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/service/DriverService.java
@@ -1,2 +1,13 @@
-package lk.easyCarRental.spring.service;public interface DriverService {
+package lk.easyCarRental.spring.service;
+
+import lk.easyCarRental.spring.dto.DriverDTO;
+
+import java.util.ArrayList;
+
+public interface DriverService {
+    void saveDriver(DriverDTO driverDTO);
+    void updateDriver(DriverDTO driverDTO);
+    void deleteDriver(String id);
+    DriverDTO searchDriver(String id);
+    ArrayList<DriverDTO> getAllDrivers();
 }
--- CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/service/impl/DriverServiceImpl.java
+++ CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/service/impl/DriverServiceImpl.java
@@ -1,34 +1,65 @@
 package lk.easyCarRental.spring.service.impl;
 
 import lk.easyCarRental.spring.dto.DriverDTO;
+import lk.easyCarRental.spring.entity.Driver;
+import lk.easyCarRental.spring.repo.DriverRepo;
 import lk.easyCarRental.spring.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 
-
+@Service
+@Transactional
 public class DriverServiceImpl implements DriverService {
+
+    @Autowired
+    DriverRepo driverRepo;
+
+    @Autowired
+    ModelMapper modelMapper;
+
     @Override
     public void saveDriver(DriverDTO driverDTO) {
-
+        if(!driverRepo.existsById(driverDTO.getDriverId())){
+            driverRepo.save(modelMapper.map(driverDTO, Driver.class));
+        }else{
+            throw new RuntimeException("Driver Already Exists");
+        }
     }
 
     @Override
     public void updateDriver(DriverDTO driverDTO) {
-
+        if(driverRepo.existsById(driverDTO.getDriverId())){
+            driverRepo.save(modelMapper.map(driverDTO,Driver.class));
+        }else {
+            throw new RuntimeException("No such a driver to update");
+        }
     }
 
     @Override
     public void deleteDriver(String id) {
-
+        if(driverRepo.existsById(id)){
+            driverRepo.deleteById(id);
+        }else{
+            throw new RuntimeException("No such a driver to update");
+        }
     }
 
     @Override
     public DriverDTO searchDriver(String id) {
-        return null;
+        if(driverRepo.existsById(id)){
+            return modelMapper.map(driverRepo.findById(id),DriverDTO.class);
+        }else{
+            throw new RuntimeException("No such a driver");
+        }
     }
 
     @Override
     public ArrayList<DriverDTO> getAllDrivers() {
-        return null;
+        return modelMapper.map(driverRepo.findAll(),new TypeToken<ArrayList<DriverDTO>>(){}.getType());
     }
 }
