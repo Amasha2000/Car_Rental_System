cd7b057b7478bd29f317e01e1c48bce9e198610d
Amasha2000 <amashagalhenage@gmail.com>
2022-07-13 18:13:56 +0530

    Added AdminDTO and Admin

---
+++ CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/dto/AdminDTO.java
@@ -0,0 +1,16 @@
+package lk.easyCarRental.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class AdminDTO {
+    private String adminId;
+    private String adminName;
+    private String adminPassword;
+}
---
+++ CarRentalSystem_BackEnd/src/main/java/lk/easyCarRental/spring/entity/Admin.java
@@ -0,0 +1,21 @@
+package lk.easyCarRental.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class Admin {
+    @Id
+    private String adminId;
+    private String adminName;
+    private String adminPassword;
+}
